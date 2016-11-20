9f89c3b09327d9433eab40eced892657d5016fda
Oleksandr Zhurbenko <ol.zhurbenko@gmail.com>
2016-11-20 14:08:36 -0800

    Added handling for 'Create New Sale' POST request

--- src/com/esit/SaleService.java
+++ src/com/esit/SaleService.java
@@ -1,15 +1,19 @@
 package com.esit;
 
 import javax.naming.NamingException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-//import javax.ws.rs.POST;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 //import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Response;
 import org.json.JSONException;
 //import org.json.JSONObject;
+import org.json.JSONObject;
 
 @Path("/SaleService")
 public class SaleService {
@@ -35,4 +39,61 @@ public class SaleService {
         return Response.status(200).entity(result).build();
       }
 
+      @POST
+      @Path("/createNewSale")
+      @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+      public Response create(
+              @FormParam("fname") String fname,
+              @FormParam("lname") String lname,
+              @FormParam("address") String address,
+              @FormParam("unitNum") String unitNum,
+              @FormParam("city") String city,
+              @FormParam("province") String province,
+              @FormParam("postalCode") String postalCode,
+              @FormParam("enbridge") String enbridge,
+              @FormParam("email") String email,
+              @FormParam("homePhone") String homePhone,
+              @FormParam("cellPhone") String cellPhone,
+              @FormParam("programType") String programType,
+              @FormParam("installationDate") String installationDate,
+              @FormParam("installationTime") String installationTime,
+              @FormParam("notes") String notes,
+              @FormParam("dateSigned") String dateSigned,
+              @FormParam("saleNumber") String saleNumber,
+              @FormParam("salesRepId") String salesRepId,
+              @FormParam("applicationNumber") String applicationNumber) {
+
+          repository = new Repository();
+          int result = repository.createNewSale(fname, lname, address, unitNum, city, province,
+                  postalCode, enbridge, email, homePhone, cellPhone, programType, installationDate, installationTime, notes,
+                  dateSigned, saleNumber, salesRepId, applicationNumber);
+
+          JSONObject jsonObj = new JSONObject();
+          jsonObj.put("fname", fname);
+          jsonObj.put("lname", lname);
+          jsonObj.put("address", address);
+          jsonObj.put("unitNum", unitNum);
+          jsonObj.put("city", city);
+          jsonObj.put("province", province);
+          jsonObj.put("postalCode", postalCode);
+          jsonObj.put("enbridge", enbridge);
+          jsonObj.put("email", email);
+          jsonObj.put("homePhone", homePhone);
+          jsonObj.put("cellPhone", cellPhone);
+          jsonObj.put("programType", programType);
+          jsonObj.put("installationDate", installationDate);
+          jsonObj.put("installationTime", installationTime);
+          jsonObj.put("notes", notes);
+          jsonObj.put("dateSigned", dateSigned);
+          jsonObj.put("saleNumber", saleNumber);
+          jsonObj.put("salesRepId", salesRepId);
+          jsonObj.put("applicationNumber", applicationNumber);
+          jsonObj.put("result", result);
+
+          if(result != 0) {
+              return Response.status(200).entity(jsonObj + "").build();
+          } else {
+              return Response.status(400).build();
+          }
+      }
 }
